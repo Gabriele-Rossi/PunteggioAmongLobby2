55ad54aee25387b19489d300f869cdc1e79a8879
Gabriele-Rossi <gabriele.rossi000@gmail.com>
2025-06-11 16:38:39 +0200

    restyling generale

---
+++ punteggioFinale.js
@@ -0,0 +1,55 @@
+TOTALE LOBBY 2: 
+
+Kropotkin = 18
+
+Bobbi = 19
+
+Saba = 4
+
+Sreper = 13.5
+
+Kendy: 0
+
+Materasso= 4
+
+Aldo = 16.5
+
+D_m3st= 9 
+
+Tastoskip = 19,5
+
+Officialyew = 7
+
+Bibi = 8.5
+
+Vanni = 9
+
+Lia = 10
+
+"FINALISTI"=
+{   TastoSkip,
+    Kropotkin,
+    Aldo,
+    Bobbi,
+    Laba,
+    Oval,
+    Tisho,
+    Polaris,
+    Travis,
+    Bomberone,
+    Kimeran,
+    Pasqu   }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
